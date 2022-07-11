bc754e3afefc26460d68e2b66720195e6d63d8b6
Loila11 <miruna_z@yahoo.com>
2022-07-11 16:13:47 +0300

    Get board areas

--- main.py
+++ main.py
@@ -30,10 +30,46 @@ def toHSV(image, diff):
         cv2.destroyAllWindows()
 
 
+def getClearImage():
+    image = cv2.imread('auxiliary_images/template_task1.jpg')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    _, gray = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY)
+
+    # Downsize image (by factor 4) to speed up morphological operations
+    gray = cv2.resize(gray, dsize=(0, 0), fx=0.25, fy=0.25)
+
+    # Morphological opening: Get rid of the stuff at the top of the ellipse
+    gray = cv2.morphologyEx(gray, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)))
+
+    # Resize image to original size
+    gray = cv2.resize(gray, dsize=(image.shape[1], image.shape[0]))
+    cv2.imwrite('Gray_removed_noise.png', gray)
+
+
+def getEllipses():
+    image = cv2.imread('Gray_removed_noise.png')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Find contours
+    cnts, hier = cv2.findContours(gray, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+    cnts = [cnts[i] for i in range(len(cnts)) if 200 < len(cnts[i]) < 10000]
+
+    # Draw found contours in input image
+    image = cv2.drawContours(image, cnts, -1, (0, 0, 255), 2)
+    # print(len(cnts))
+
+    # Downsize image
+    out_image = cv2.resize(image, dsize=(0, 0), fx=0.25, fy=0.25)
+    cv2.imwrite('test_ellipses.png', out_image)
+
+    return cnts
+
+
 def diffOpenCV(path, image_name):
     aux_image = cv2.imread('auxiliary_images/template_task1.jpg')
     # aux_image = cv2.cvtColor(aux_image, cv2.COLOR_BGR2HSV)
-    # aux_image = cv2.cvtColor(aux_image, cv2.COLOR_BGR2GRAY)
+    # aux_gray = cv2.cvtColor(aux_image, cv2.COLOR_BGR2GRAY)
 
     image = cv2.imread(path + image_name)
     # image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -41,19 +77,20 @@ def diffOpenCV(path, image_name):
 
     diff = cv2.absdiff(image, aux_image)
     # diff = cv2.cvtColor(diff, cv2.COLOR_BGR2HSV)
-    # gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
 
     # canvas = np.zeros_like(image, np.uint8)
     # canvas[diff > 1] = image[diff > 1]
 
     # toHSV(image, diff)
 
-    cv2.imwrite('test_diff.png', diff)
-    # cv2.imwrite('evaluation/Task1/' + image_name, diff)
+    # cv2.imwrite('test_diff_gray.png', gray)
+    # cv2.imwrite('evaluation/Task1/.' + image_name, gray)
 
 
 def task1(path):
-    # 'evaluation/Task1/...'
+    # getClearImage()
+    areas = getEllipses()
     path += '/Task1/'
 
     for i in range(1, 2):
