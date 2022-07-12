d5d3d1ce65b12d1e9cdde48d9c1a77f196adb490
Loila11 <miruna_z@yahoo.com>
2022-07-12 18:26:14 +0300

    Count number of darts on the board

--- task1.py
+++ task1.py
@@ -5,6 +5,7 @@ from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 
 from matplotlib import pyplot as plt
+from IPython import display
 
 
 def getClearImage(origin, destination):
@@ -127,15 +128,62 @@ def toHSV(image, diff):
     return mask
 
 
+def getDartsAreas(best_squares):
+    best_best_squares = []
+    for square in best_squares:
+        overlaps = False
+        for i in range(len(best_best_squares)):
+            best_square = best_best_squares[i]
+            if square[0][0] >= best_square[1][0] or \
+               square[1][0] <= best_square[0][0] or \
+               square[1][1] <= best_square[0][1] or \
+               square[0][1] >= best_square[1][1]:
+                continue
+
+            best_best_squares[i] = ((min(square[0][0], best_square[0][0]), min(square[0][1], best_square[0][1])),
+                                    (max(square[1][0], best_square[1][0]), max(square[1][1], best_square[1][1])),
+                                    min(square[2], best_square[2]))
+            overlaps = True
+
+        if not overlaps:
+            best_best_squares += [square]
+
+    return best_best_squares
+
+
+def drawRectangle(mask, point1, point2):
+    clone = cv2.cvtColor(mask.copy(), cv2.COLOR_GRAY2BGR)
+    cv2.rectangle(clone, point1, point2, (0, 255, 0), 2)
+    clone = clone[:, :, ::-1]
+    plt.imshow(clone)
+    plt.pause(0.1)
+    display.clear_output(wait=True)
+
+
 def countDarts(path):
     image = cv2.imread(path)
-    # image = cv2.resize(image, dsize=(0, 0), fx=0.2, fy=0.2)
+    image = cv2.resize(image, dsize=(0, 0), fx=0.2, fy=0.2)
     mask = toHSV(image, image)
 
-    white = np.count_nonzero(np.all(mask == [255, 255, 255], axis=2))
+    best_squares = []
 
-    # cv2.imshow('mask', mask)
-    # cv2.waitKey(0)
+    size = 30
+    step = 10
+    for y in range(50, mask.shape[0] - size, step):
+        for x in range(50, mask.shape[1] - size, step):
+            window = mask[y:y + size, x:x + size]
+            white = np.count_nonzero(window)
+            score = 100000 if white == 0 else size * size / white
+            if score < 9:
+                best_squares += [((x, y), (x + size, y + size), score)]
+
+    best_best_squares = getDartsAreas(best_squares)
+    print(best_best_squares)
+
+    # for square in best_best_squares:
+    #     drawRectangle(mask, square[0], square[1])
+
+    return len(best_best_squares)
 
 
 def template_matching(path):
@@ -181,9 +229,9 @@ def task1(path):
     for i in range(1, 2):
         image_name = path
         if i < 10:
-            image_name = '0'
+            image_name += '0'
         image_name += str(i) + '.jpg'
 
-        # countDarts(image_name)
+        countDarts(image_name)
         # template_matching(image_name)
         # getDiff(image_name)
